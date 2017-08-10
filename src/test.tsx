2ca9f8f13eb53846bfd2e56c3a7dc9f9686b91f5
AlexanderFadeev <1alexking1@gmail.com>
2017-08-10 18:13:41 +0300

    Added levels

---
+++ src/test.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="sprites/test.png" width="512" height="512"/>
+ <tile id="0" type="Box"/>
+ <tile id="1" type="Wall"/>
+ <tile id="2" type="Character"/>
+</tileset>
